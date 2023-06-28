150c8abb1b5edb8cdd41c525f388ba122633b26b
NetAcad DEVASC <changeme@example.com>
2023-06-28 22:19:55 +0000

    agregados script de integrantes

---
+++ Script-nombres.py
@@ -0,0 +1,5 @@
+integrantes = ["- Bastian Levillan", "- Bastian Munoz", "- Braian Manriquez"]
+
+print("Lista de nombres y apellidos de los integrantes del grupo:")
+for integrante in integrantes:
+    print(integrante)
